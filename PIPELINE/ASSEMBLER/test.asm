e17cf523602b1e767c96bfa3b3474b966fb26925
Bruno Genero <generobruno@gmail.com>
2024-04-11 15:56:14 -0300

    cambios en reset de pipeline_regs

--- PIPELINE/ASSEMBLER/test.asm
+++ PIPELINE/ASSEMBLER/test.asm
@@ -1,6 +1,6 @@
 xori $r1, $r0, 0003     # 00hex
 xori $r2, $r0, 0004     # 04hex
-j 20                    # 08hex: Control Hazard -> Jump to Next1
+j 20                    
 xori $r1, $r0, 0001     # 0Chex: Next2
 xori $r2, $r0, 0001     # 10hex
 subu $r3, $r2, $r1      # 14hex: Next1 -> Data Hazard (Forwarding)
@@ -9,7 +9,7 @@ addu $r4, $r1, $r2      # 1Chex:
 sw $r4, 16($r3)         # 20hex: Data Hazard (Forwarding)
 lw $r5, 16($r3)         # 24hex
 slt $r6, $r1, $r5       # 28hex: Data Hazard (Stalling and Forwarding)
-slt $r4, 16($r3)        # 2Chex
+sw $r4, 16($r3)        # 2Chex
 xori $r4, $r3, 0001     # 30hex: No Data Hazard (Stalling)
 xori $r6, $r6, 0001     # 34hex
 halt
--- PIPELINE/ASSEMBLER/test2.asm
+++ PIPELINE/ASSEMBLER/test2.asm
@@ -1,18 +1,18 @@
-xori $r1, $r0, 0003     # 00hex
-xori $r2, $r0, 0004     # 04hex
-j 56                    # 08hex: Jump to Next3
-xori $r1, $r0, 0001     # 0Chex: Next2
-xori $r2, $r0, 0001     # 10hex
-subu $r3, $r2, $r1      # 14hex: Next1 
-bne $r1, $r2, 65532     # 18hex
-addu $r4, $r1, $r2      # 1Chex
-sw $r4, 16($r3)         # 20hex
-lw $r5, 16($r3)         # 24hex
-slt $r6, $r1, $r5       # 28hex
-slt $r4, 16($r3)        # 2Chex
-xori $r4, $r3, 0001     # 30hex
-xori $r6, $r6, 0001     # 34hex
-addi $r15, $r0, 65535    # 38hex: Next3 -> Add FFFF in reg[15]
-sw $r15, 24($r13)       # 3Chex
-lhu $r30, 24($r13)      # 40hex: Test LHU
+xori $r1, $r0, 0003     
+xori $r2, $r0, 0004     
+j 56
+xori $r1, $r0, 0001     
+xori $r2, $r0, 0001
+subu $r3, $r2, $r1        
+bne $r1, $r2, 65532
+addu $r4, $r1, $r2 
+sw $r4, 16($r3)    
+lw $r5, 16($r3)    
+slt $r6, $r1, $r5  
+slt $r4, 16($r3)   
+xori $r4, $r3, 0001
+xori $r6, $r6, 0001
+addi $r15, $r0, 65535    
+sw $r15, 24($r13)  
+lhu $r30, 24($r13)
 halt
